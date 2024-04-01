3c5f9b429a358cd1b0ddb80c4fab1130014f037d
Yifeng Wang <doodlewind@toeverything.info>
2024-04-01 22:26:41 +0800

    refactor: abstract workspace layout

--- packages/client/src/App.vue
+++ packages/client/src/App.vue
@@ -1,39 +1,7 @@
 <template>
-  <EditorProvider>
-    <n-message-provider>
-      <n-layout position="absolute">
-        <n-layout-header style="height: 40px" bordered>
-          <TopBar />
-        </n-layout-header>
-        <n-layout has-sider position="absolute" style="top: 40px;">
-          <n-layout-sider
-            collapse-mode="width"
-            :collapsed-width="120"
-            :width="240"
-            show-trigger="bar"
-            bordered
-          >
-            <Sidebar />
-          </n-layout-sider>
-          <n-layout content-style="padding: 24px;">
-            <EditorContainer />
-          </n-layout>
-        </n-layout>
-      </n-layout>
-    </n-message-provider>
-  </EditorProvider>
+  <WorkspaceLayout />
 </template>
 
 <script setup>
-import {
-  NMessageProvider,
-  NLayout,
-  NLayoutSider,
-  NLayoutHeader,
-} from 'naive-ui';
-import EditorProvider from './components/EditorProvider.vue';
-import Sidebar from './components/Sidebar.vue';
-import TopBar from './components/TopBar.vue';
-import EditorContainer from './components/EditorContainer.vue';
-import './index.css';
+import WorkspaceLayout from './components/WorkspaceLayout.vue';
 </script>
---
+++ packages/client/src/components/WorkspaceLayout.vue
@@ -0,0 +1,38 @@
+<template>
+  <EditorProvider>
+    <NMessageProvider>
+      <NLayout position="absolute">
+        <NLayoutHeader style="height: 40px" bordered>
+          <TopBar />
+        </NLayoutHeader>
+        <NLayout hasSider position="absolute" style="top: 40px">
+          <NLayoutSider
+            collapseMode="width"
+            :collapsedWidth="30"
+            :width="240"
+            showTrigger="bar"
+            bordered
+          >
+            <Sidebar />
+          </NLayoutSider>
+          <NLayout contentStyle="padding: 24px;">
+            <EditorContainer />
+          </NLayout>
+        </NLayout>
+      </NLayout>
+    </NMessageProvider>
+  </EditorProvider>
+</template>
+
+<script setup>
+import {
+  NMessageProvider,
+  NLayout,
+  NLayoutSider,
+  NLayoutHeader,
+} from 'naive-ui';
+import EditorProvider from './EditorProvider.vue';
+import Sidebar from './Sidebar.vue';
+import TopBar from './TopBar.vue';
+import EditorContainer from './EditorContainer.vue';
+</script>
