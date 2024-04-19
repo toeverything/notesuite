5116ad89af3bd215e8ba19727f1ab5ce039203c4
Yifeng Wang <doodlewind@toeverything.info>
2024-04-19 11:56:55 +0800

    feat: add webdav scaffold

--- packages/drive/src/index.ts
+++ packages/drive/src/index.ts
@@ -1,1 +1,21 @@
-// TODO
+import { v2 as webdav } from 'webdav-server';
+
+const server = new webdav.WebDAVServer();
+
+server.rootFileSystem().addSubTree(
+  server.createExternalContext(),
+  {
+    folder1: {
+      'file1.txt': webdav.ResourceType.File,
+      folder2: {
+        'file2.txt': webdav.ResourceType.File,
+      },
+    },
+    'root.txt': webdav.ResourceType.File,
+  },
+  () => {}
+);
+
+server.start(() =>
+  console.log('WebDAV server started on http://localhost:1900')
+);
