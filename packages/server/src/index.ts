965c918b0790171378f5f65c1f10fa9b504087ce
Yifeng Wang <doodlewind@toeverything.info>
2024-05-22 01:00:09 +0800

    chore: debug

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -2,6 +2,8 @@ import { registerAPI } from './api.js';
 import { initWebDAVServer } from './drive/drive.js';
 import { initAppContext, initWSServer } from './utils.js';
 
+console.log('Starting server...');
+console.log('NODE_OPTIONS:', process.env.NODE_OPTIONS);
 console.log(111);
 
 const context = await initAppContext();
