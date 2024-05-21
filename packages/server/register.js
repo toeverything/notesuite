71ea35a97028b9a5a84eb1af01300d0e399bb782
Yifeng Wang <doodlewind@toeverything.info>
2024-05-22 01:07:51 +0800

    chore: debug

--- packages/server/register.js
+++ packages/server/register.js
@@ -3,4 +3,3 @@ import { pathToFileURL } from 'node:url';
 
 register('ts-node/esm', pathToFileURL('./'));
 console.log('Registered ts-node/esm');
-console.log('NODE_OPTIONS:', process.env.NODE_OPTIONS);
--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -2,17 +2,7 @@ import { registerAPI } from './api.js';
 import { initWebDAVServer } from './drive/drive.js';
 import { initAppContext, initWSServer } from './utils.js';
 
-console.log('Starting server...');
-console.log('NODE_OPTIONS:', process.env.NODE_OPTIONS);
-console.log(111);
-
 const context = await initAppContext();
-
-console.log('Server...', context.port);
 registerAPI(context);
-console.log('API registered');
-
 initWSServer(context);
-console.log('WebSocket server started');
-
 initWebDAVServer(context);
