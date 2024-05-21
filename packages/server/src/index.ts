12543c0d5c48ce68ca357d6e6cd3a3f5a560b81a
Yifeng Wang <doodlewind@toeverything.info>
2024-05-22 00:46:47 +0800

    chore: test

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -4,6 +4,11 @@ import { initAppContext, initWSServer } from './utils.js';
 
 const context = await initAppContext();
 
+console.log('Server...', context.port);
 registerAPI(context);
+console.log('API registered');
+
 initWSServer(context);
+console.log('WebSocket server started');
+
 initWebDAVServer(context);
