8f94fdb95419a0d0f33331b481ead0819b8501bc
Yifeng Wang <doodlewind@toeverything.info>
2024-05-21 22:24:49 +0800

    feat: add create workspace test

--- tests/basic.spec.ts
+++ tests/basic.spec.ts
@@ -3,20 +3,25 @@ import { TestAgent } from './common/agent.js';
 
 test.describe('basic test', () => {
   const agent = new TestAgent();
-
-  test.beforeAll(() =>
-    agent.start({
-      name: 'test-basic',
-      webPort: 5173,
-      backendPort: 3000,
-    })
-  );
+  const options = {
+    name: 'test-basic',
+    webPort: 5173,
+    backendPort: 3000,
+  };
+  test.beforeAll(() => agent.start(options));
 
   test('local server works', async ({ page }) => {
-    await page.goto('http://localhost:5173');
-    const title = await page.title();
-    expect(title).toBe('Note App');
+    await page.goto(agent.web.baseUrl);
+    expect(await page.title()).toBe('Note App');
   });
 
-  test.afterAll(async () => await agent.stop());
+  test('can crete workspace', async ({ page }) => {
+    await page.goto(agent.web.baseUrl);
+    await page.getByPlaceholder('Workspace name').click();
+    await page.getByPlaceholder('Workspace name').fill('hello');
+    await page.getByRole('button', { name: 'Create' }).click();
+    await expect(page.getByText('Test Client')).toBeVisible();
+  });
+
+  test.afterAll(() => agent.stop());
 });
--- tests/common/agent.ts
+++ tests/common/agent.ts
@@ -7,10 +7,31 @@ interface StartOptions {
   backendPort: number;
 }
 
+class WebAgentInterface {
+  constructor(private agent: TestAgent) {}
+
+  get baseUrl() {
+    return `http://localhost:${this.agent.options.webPort}`;
+  }
+}
+
+class MockAgentInterface {
+  constructor(private agent: TestAgent) {}
+}
+
 export class TestAgent {
   runner = new AppRunner();
+  web = new WebAgentInterface(this);
+  mock = new MockAgentInterface(this);
+
+  options = {
+    name: '',
+    webPort: 0,
+    backendPort: 0,
+  };
 
   async start(options: StartOptions) {
+    this.options = options;
     await Promise.all([
       this.runner.startServer(options.backendPort, options.name),
       this.runner.startWeb(options.webPort, options.backendPort),
